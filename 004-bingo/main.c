9f0dc630a8e62c3dfdd3b2f57b432c755c3e059f
rkbrian <rkbriankong@gmail.com>
2021-12-06 05:22:11 +0000

    p1 progress

--- 004-bingo/main.c
+++ 004-bingo/main.c
@@ -2,69 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "linly.h"
-
-void mygaming(board_list *head, int *num_call, int calls)
-{
-        board_list *current = NULL;
-        int i = 0, j, k, list_i = 0, flag, win_num = 0, sum, x, y;
-
-        current = head;
-        while (i < calls)
-        {
-                while (current)
-                {
-                        flag = 0;
-                        for (j = 0; j < 5; j++)
-                        {
-                                for (k = 0; k < 5; k++)
-                                {
-                                        if (current->array[j][k] == num_call[i])
-                                        {
-                                                current->marks[j][k] = 1, flag = 1, win_num = current->array[j][k];
-                                                break;
-                                        }
-                                }
-                                if (flag == 1)
-                                        break;
-                        }
-			printf("%d\n", win_num);
-                        flag = 0, sum = 0;
-			if (j < 5 && k < 5)
-                        {
-                                if (current->marks[j][0]+current->marks[j][1]+current->marks[j][2]+current->marks[j][3]+current->marks[j][4] == 5 || current->marks[0][k]+current->marks[1][\
-					    k]+current->marks[2][k]+current->marks[3][k]+current->marks[4][k] == 5)
-                                {
-                                        printf("winner board: %d\n", list_i);
-                                        for (x = 0; x < 5; x++)
-                                        {
-                                                for (y = 0; y < 5; y++)
-                                                {
-                                                        if (current->marks[x][y] == 0)
-                                                                sum += current->array[x][y];
-                                                }
-                                        }
-                                        printf("winner score: %d\n", sum * win_num), flag = 2;
-                                        break;
-                                }
-                        }
-                        if (flag == 2)
-                                break;
-                        list_i++;
-                }
-                if (flag == 2)
-                        break;
-        }
-}
 
 int main(void)
 {
 	FILE *fp = fopen("test_data1", "r");
-	char *lines = NULL, *token;
-	size_t len = 0;
+	char *lines = NULL, *token, *toten;
+	size_t len = 0, leng = 0;
 	ssize_t reading;
-	int num_call[256], i = 0, j, k = 0, calls = 0;
-	board_list *head, *current = NULL;
+	int num_call[256], i = 0, j, k, m, calls = 0, flag;
+	int bingo[5][5], logic_board[5][5], win_num, sum;
 
 	if (fp == NULL) /* save calls */
 		exit(EXIT_FAILURE);
@@ -74,37 +20,68 @@ int main(void)
 		while (token)
 			num_call[i] = atoi(token), calls++, token = strtok(NULL, ","), i++;
 	}
-	fclose(fp), i = 0, token = NULL;
+	fclose(fp), i = 0;
 	if (lines)
 		free(lines);
-	fp = fopen("test_data2", "r"); /* save boards */
-	head = malloc(sizeof(board_list *)), current = head;
-	while ((reading = getline(&lines, &len, fp)) != -1)
+	fp = fopen("test_data2", "r"); /* check winner */
+	while ((reading = getline(&lines, &leng, fp)) != -1)
 	{
-		if (i == 0)
-			token = strtok(lines, " ");
 		if ((i + 1) % 6 != 0)
 		{
+			toten = strtok(lines, " ");
+			printf("debugger: %s\n", toten);
 			for (j = 0; j < 5; j++)
-				current->array[k][j] = atoi(token), current->marks[k][j] = 0;
+			{
+				bingo[k][j] = atoi(toten), logic_board[k][j] = 0;
+				if (j < 4)
+					toten = strtok(NULL, " ");
+				fflush(stdout), printf("%d, ", bingo[k][j]);
+			}
 			k++;
 		}
 		else
-			k = 0, current = current->next, current = malloc(sizeof(board_list *));
-		if (i != 0)
-			token = strtok(NULL, " ");
+		{
+			for (m = 0; m < calls; m++)
+			{
+				for (k = 0; k < 5; k++)
+				{
+					flag = 0;
+					for (j = 0; j < 5; j++)
+					{
+						if (num_call[m] == bingo[k][j])
+						{
+							logic_board[k][j] = 1;
+							if (logic_board[k][0]+logic_board[k][1]+logic_board[k][2]+logic_board[k][3]+logic_board[k][4] == 5
+							    || logic_board[0][j]+logic_board[1][j]+logic_board[2][j]+logic_board[3][j]+logic_board[4][j] == 5)
+							{
+								win_num = num_call[m], flag = 1;
+								break;
+							}
+						}
+					}
+					if (flag == 1)
+						break;
+				}
+				if (flag == 1)
+				{
+					sum = 0;
+					for (k = 0; k < 5; k++)
+					{
+						for (j = 0; j < 5; j++)
+						{
+							if (logic_board[k][j] == 0)
+								sum++;
+						}
+					}
+					fflush(stdout), printf("win number: %d\nsum: %d\nscore: %d\n", win_num, sum, win_num * sum);
+					return (0);
+				}
+			}
+		}
 		i++;
 	}
-	printf("debugger\n");
-	fclose(fp), token = NULL;
+	fclose(fp), token = NULL, toten = NULL;
 	if (lines)
 		free(lines);
-	mygaming(head, num_call, calls);
-	while (head)
-	{
-		current = head->next;
-		free(head);
-		head = current;
-	}
 	return (0);
 }
