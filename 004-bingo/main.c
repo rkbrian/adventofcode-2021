92671933fbfee9fdda1f1f10cb3835f37e80195b
Ran Kong <rkbriankong@gmail.com>
2021-12-06 00:26:17 -0600

    test

--- 004-bingo/main.c
+++ 004-bingo/main.c
@@ -6,10 +6,10 @@
 int main(void)
 {
 	FILE *fp = fopen("test_data1", "r");
-	char *lines = NULL, *token, *toten;
+	char *lines = NULL, *linez = NULL *token, *toten;
 	size_t len = 0, leng = 0;
 	ssize_t reading;
-	int num_call[256], i = 0, j, k, m, calls = 0, flag;
+	int num_call[256], i = 0, j, k, m, calls = 0, flag, x, y;
 	int bingo[5][5], logic_board[5][5], win_num, sum;
 
 	if (fp == NULL) /* save calls */
@@ -24,11 +24,11 @@ int main(void)
 	if (lines)
 		free(lines);
 	fp = fopen("test_data2", "r"); /* check winner */
-	while ((reading = getline(&lines, &leng, fp)) != -1)
+	while ((reading = getline(&linez, &leng, fp)) != -1)
 	{
 		if ((i + 1) % 6 != 0)
 		{
-			toten = strtok(lines, " ");
+			toten = strtok(linez, " ");
 			printf("debugger: %s\n", toten);
 			for (j = 0; j < 5; j++)
 			{
@@ -43,26 +43,25 @@ int main(void)
 		{
 			for (m = 0; m < calls; m++)
 			{
+				flag = 0, x = -1, y = -1;
 				for (k = 0; k < 5; k++)
 				{
-					flag = 0;
 					for (j = 0; j < 5; j++)
 					{
 						if (num_call[m] == bingo[k][j])
 						{
-							logic_board[k][j] = 1;
-							if (logic_board[k][0]+logic_board[k][1]+logic_board[k][2]+logic_board[k][3]+logic_board[k][4] == 5
-							    || logic_board[0][j]+logic_board[1][j]+logic_board[2][j]+logic_board[3][j]+logic_board[4][j] == 5)
-							{
-								win_num = num_call[m], flag = 1;
+							logic_board[k][j] = 1, flag = 1;
+							if (flag == 1)
 								break;
-							}
 						}
 					}
 					if (flag == 1)
 						break;
 				}
-				if (flag == 1)
+				if (flag == 1 && (logic_board[k][0]+logic_board[k][1]+logic_board[k][2]+logic_board[k][3]+logic_board[k][4] == 5
+				    || logic_board[0][j]+logic_board[1][j]+logic_board[2][j]+logic_board[3][j]+logic_board[4][j] == 5))
+					win_num = num_call[m], flag = 2;
+				if (flag == 2)
 				{
 					sum = 0;
 					for (k = 0; k < 5; k++)
@@ -81,7 +80,7 @@ int main(void)
 		i++;
 	}
 	fclose(fp), token = NULL, toten = NULL;
-	if (lines)
-		free(lines);
+	if (linez)
+		free(linez);
 	return (0);
 }
